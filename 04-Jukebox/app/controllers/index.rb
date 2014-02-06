cdb86e197861fd9bc0816805d7283d4b6fc15a27
Estelleabn <estelleaubouin@gmail.com>
2014-02-06 14:31:41 +0100

    solution Jukebox

--- 04-Jukebox/app/controllers/index.rb
+++ 04-Jukebox/app/controllers/index.rb
@@ -1,4 +1,5 @@
 get '/' do
+
   erb :index
 end
 
@@ -10,10 +11,12 @@ post '/' do
   # Create a new album and add it to the $albums array 
   # You should use only one parameter of the params hash named :album
   # You should use only Album#new and Album#save
+  new_album = Album.new(params[:album])
+
   erb :index
 end
 
-get '/:id' do 
-  # Find the album with index :id and transmit it to the show view
-  erb :show
+get '/:id' do
+@album = Album.find(params[:id].to_i)
+erb :show
 end
